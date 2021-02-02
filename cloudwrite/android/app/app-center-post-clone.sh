e8da068aa7909f65a39fa2d3e2e854bc1e53d1d6
Patrycjusz Nikrewicz <patrycjusz.nikrewicz@gmail.com>
2021-02-02 13:27:33 +0100

    changes

--- cloudwrite/android/app/app-center-post-clone.sh
+++ cloudwrite/android/app/app-center-post-clone.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
-#Place this script in project/android/app/
+# place this script in project/android/app/
 cd ..
 # fail if any command fails
 set -e
 # debug log
 set -x
+
 cd ..
+# choose a different release channel if you want - https://github.com/flutter/flutter/wiki/Flutter-build-release-channels
+# stable - recommended for production
+
 git clone -b master https://github.com/Nikron9/CloudWriteApp.git
 export PATH=`pwd`/flutter/bin:$PATH
+
 flutter channel stable
 flutter doctor
-echo "Installed flutter to `pwd`/flutter"
 flutter build apk --release
-#copy the APK where AppCenter will find it
+
+# copy the APK where AppCenter will find it
 mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
